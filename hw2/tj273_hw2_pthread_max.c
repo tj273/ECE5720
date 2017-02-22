dc2e92c7eff45da788fc12a7680705b505d2aa29
tj273 <tj273@cornell.edu>
2017-02-21 22:08:52 -0500

    hw2 update

---
+++ hw2/tj273_hw2_pthread_max.c
@@ -0,0 +1,17 @@
+/* ECE 5720 hw2 pthread
+ * Name  : Tianze Jiang
+ * Netid : tj273
+ * 
+ * % gcc -o tj273_hw2_pthread tj273_hw2_pthread_max.c -lrt
+ * % ./tj273_hw2_pthread
+*/
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
+
+void main()
+{
+  
+}
