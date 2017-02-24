13e80164ff3576cff7788642c641aec1ffef0dae
tj273 <tj273@cornell.edu>
2017-02-24 18:53:34 -0500

    pthread finished

---
+++ hw2/tj273_hw2_openmp_sort.c
@@ -0,0 +1,24 @@
+/* ECE 5720 hw2 openmp
+ * Name  : Tianze Jiang
+ * Netid : tj273
+ * 
+ * % gcc tj273_hw2_openmp_sort.c tj273_openmp_sort -fopenmp
+ * % ./tj273_openmp_sort
+*/
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+
+#define N 4096
+#define NUM_THREADS 16
+#define BILLION 1000000000L
+
+void main()
+{
+  
+  
+  return;
+}
