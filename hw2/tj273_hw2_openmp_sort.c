6da7e34b46b598cb6fc5962d46a70c85a375f15f
tj273 <tj273@cornell.edu>
2017-02-25 01:05:34 -0500

    openmp finished

--- hw2/tj273_hw2_openmp_sort.c
+++ hw2/tj273_hw2_openmp_sort.c
@@ -2,7 +2,7 @@
  * Name  : Tianze Jiang
  * Netid : tj273
  * 
- * % gcc tj273_hw2_openmp_sort.c tj273_openmp_sort -fopenmp
+ * % gcc tj273_hw2_openmp_sort.c -o tj273_openmp_sort -fopenmp
  * % ./tj273_openmp_sort
 */
 
@@ -12,13 +12,285 @@
 #include <time.h>
 #include <omp.h>
 
-#define N 4096
-#define NUM_THREADS 16
+#define N 4
+#define NUM_THREADS 8
 #define BILLION 1000000000L
+
+void Merge(int *array,int *left_array,int leftCount,int *right_array,int rightCount, int *idx, int *left_idx, int *right_idx);
+void MergeSort(int *array, int *idx, int n);
+void MergeSort_omp(int *array, int *idx, int n, int ava_num_threads);
+void RowSort_omp(int **A, int row);
+void RowSort_sequential(int **A, int row);
+void PrintArray(int **A);
+
+uint64_t time_sorto = 0;
+uint64_t time_sorts = 0;
+int **A;
+int **temp;
 
 void main()
 {
+  int i, j;
   
+  A = (int **) malloc (N * sizeof(int *));
+  for ( i = 0; i < N; i++ )
+    A[i] = (int *) malloc (N * sizeof(int));
+    
+  temp = (int **) malloc (N * sizeof(int *));
+  for ( i = 0; i < N; i++ )
+    temp[i] = (int *) malloc (N * sizeof(int));
+    
+  srand(3);
+  for ( i = 0; i < N; i++ )
+  	for ( j = 0; j < N; j++ )
+  		A[i][j] = rand()%100 - 50;
+      
+  PrintArray(A);
+  
+  for (i = 0; i < N-1; i++ )
+    RowSort_omp( A, i );
+  
+  PrintArray(A);
+  
+  printf("Time elapsed to sort all columns in entire matrix    with openmp is %15llu ns\n", (long long unsigned)time_sorto);
+  
+  for ( i = 0; i < N; i++ )
+    for ( j = 0; j < N; j++ )
+      A[i][j] = rand()%100 - 50;
+
+  PrintArray(A);
+  
+  for (i = 0; i < N-1; i++ )
+    RowSort_sequential( A, i );
+  
+  PrintArray(A);
+  
+  printf("Time elapsed to sort all columns in entire matrix without openmp is %15llu ns\n", (long long unsigned)time_sorts);
+
+  for ( i = 0; i < N; i++ )
+    free(A[i]);
+  
+  free(A);
+
+  return;
+}
+
+void Merge(int *array,int *left_array, int leftCount, int *right_array, int rightCount, int *idx, int *left_idx, int *right_idx)
+{
+	int lp,rp,ap;
+  
+	lp = 0; // left  array pointer
+  rp = 0; // right array pointer
+  ap = 0; // new   array pointer
+
+
+	while( (lp < leftCount) && (rp < rightCount)) {
+		if(left_array[lp] < right_array[rp]) {
+      array[ap] = left_array[lp];
+      idx[ap] = left_idx[lp];
+      ap = ap + 1;
+      lp = lp + 1;
+    }
+		else {
+      array[ap] = right_array[rp];
+      idx[ap] = right_idx[rp];
+      ap = ap + 1;
+      rp = rp + 1;
+    }
+	}
+  
+	while(lp < leftCount) {
+    array[ap] = left_array[lp];
+    idx[ap] = left_idx[lp];
+    ap = ap + 1;
+    lp = lp + 1;
+  }
+	while(rp < rightCount) {
+    array[ap] = right_array[rp];
+    idx[ap] = right_idx[rp];
+    ap = ap + 1;
+    rp = rp + 1;
+  }
+}
+
+void MergeSort(int *array, int *idx, int n)
+{
+	int mid, i;
+  int *left_array, *right_array;
+  int *left_idx, *right_idx;
+	if(n < 2) 
+    return; 
+
+	mid = n/2;  // find the mid index. 
+
+	left_array = (int*)malloc(mid*sizeof(int)); 
+	right_array = (int*)malloc((n- mid)*sizeof(int)); 
+  
+  left_idx = (int*)malloc(mid*sizeof(int)); 
+	right_idx = (int*)malloc((n- mid)*sizeof(int)); 
+	
+	for(i = 0;i<mid;i++) {
+    left_array[i] = array[i]; // creating left subarray
+    left_idx[i] = idx[i];
+  }
+  
+	for(i = mid;i<n;i++) {
+    right_array[i-mid] = array[i]; // creating right subarray
+    right_idx[i-mid] = idx[i];
+  }
+  
+  MergeSort(left_array, left_idx, mid);  // sorting the left subarray
+  MergeSort(right_array, right_idx, n-mid);  // sorting the right subarray
+  
+	Merge(array, left_array, mid, right_array, n-mid, idx, left_idx, right_idx);  // Merging L and R into A as sorted list.
+  
+  free(left_array);
+  free(right_array);
+  
+  free(left_idx);
+  free(right_idx);
+}
+
+void MergeSort_omp(int *array, int *idx, int n, int ava_num_threads)
+{ 
+  if (ava_num_threads < 2) {
+    MergeSort(array, idx, n);
+    return;
+  }
+     
+	int mid, i;
+  int *left_array, *right_array;
+  int *left_idx, *right_idx;
+	if(n < 2) 
+    return; 
+
+	mid = n/2;  // find the mid index. 
+
+	left_array = (int*)malloc(mid*sizeof(int)); 
+	right_array = (int*)malloc((n- mid)*sizeof(int)); 
+  
+  left_idx = (int*)malloc(mid*sizeof(int)); 
+	right_idx = (int*)malloc((n- mid)*sizeof(int)); 
+	
+	for(i = 0;i<mid;i++) {
+    left_array[i] = array[i]; // creating left subarray
+    left_idx[i] = idx[i];
+  }
+  
+	for(i = mid;i<n;i++) {
+    right_array[i-mid] = array[i]; // creating right subarray
+    right_idx[i-mid] = idx[i];
+  }
+  
+  omp_set_dynamic(0);
+  omp_set_nested(1);
+  
+  #pragma omp parallel sections 
+  {
+    #pragma omp section
+    MergeSort_omp(left_array, left_idx, mid, ava_num_threads/2);  // sorting the left subarray
+    #pragma omp section
+    MergeSort_omp(right_array, right_idx, n-mid, ava_num_threads-ava_num_threads/2);  // sorting the right subarray
+  }
+  
+  Merge(array, left_array, mid, right_array, n-mid, idx, left_idx, right_idx);  // Merging L and R into A as sorted list.
+  
+  free(left_array);
+  free(right_array);
+  
+  free(left_idx);
+  free(right_idx);
+  
+  return;
+}
+
+void RowSort_omp(int **A, int row)
+{
+  int i, j;
+  
+  int idx[N-row];
+  for ( i = 0; i < N-row; i++ )
+    idx[i] = i + row;
+  
+  int array[N-row];
+  for ( i = 0; i < N-row; i++ )
+    array[i] = A[i+row][row];
+  
+  uint64_t diff;
+  struct timespec start, end;
+  
+  int available_threads;
+  #pragma omp parallel
+  {
+    #pragma omp master
+    {
+      available_threads = omp_get_num_threads();
+    }
+  }
+    
+  clock_gettime(CLOCK_MONOTONIC, &start);	/* mark start time */
+  
+  MergeSort_omp(array, idx, N-row, available_threads);
+  
+  clock_gettime(CLOCK_MONOTONIC, &end);	/* mark the end time */
+  diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+  time_sorto = time_sorto + diff;
+  
+  for ( i = row; i < N; i++ )
+  	for ( j = 0; j < N; j++ )
+  		temp[i][j] = A[i][j]; 
+  
+  for ( i = row; i < N; i++ )
+  	for ( j = 0; j < N; j++ )
+  		A[i][j] = temp[idx[i-row]][j];
+    
+  return;
+}
+
+void RowSort_sequential(int **A, int row)
+{
+  int i, j;
+  
+  int idx[N-row];
+  for ( i = 0; i < N-row; i++ )
+    idx[i] = i + row;
+  
+  int array[N-row];
+  for ( i = 0; i < N-row; i++ )
+    array[i] = A[i+row][row];
+  
+  uint64_t diff;
+  struct timespec start, end;
+    
+  clock_gettime(CLOCK_MONOTONIC, &start);	/* mark start time */
+  
+  MergeSort(array, idx, N-row);
+  
+  clock_gettime(CLOCK_MONOTONIC, &end);	/* mark the end time */
+  diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+  time_sorts = time_sorts + diff;
+  
+  for ( i = row; i < N; i++ )
+  	for ( j = 0; j < N; j++ )
+  		temp[i][j] = A[i][j]; 
+  
+  for ( i = row; i < N; i++ )
+  	for ( j = 0; j < N; j++ )
+  		A[i][j] = temp[idx[i-row]][j];
+    
+  return;
+}
+
+void PrintArray(int **A)
+{
+  int i,j;
+  
+  for ( i = 0; i < N; i++ ) {
+  	for ( j = 0; j < N; j++ )
+  		printf ("%3d  ", A[i][j]); 
+    printf("\n");
+  }
+  printf("\n");
   
   return;
 }
--- hw2/tj273_hw2_pthread_max.c
+++ hw2/tj273_hw2_pthread_max.c
@@ -2,16 +2,204 @@
  * Name  : Tianze Jiang
  * Netid : tj273
  * 
- * % gcc -o tj273_hw2_pthread tj273_hw2_pthread_max.c -lrt
- * % ./tj273_hw2_pthread
+ * % gcc tj273_hw2_pthread_max.c -O3 -o tj273_pthread_max -lpthread
+ * % ./tj273_pthread_max
 */
 
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <time.h>
+#include <pthread.h>
+
+#define N 32768
+#define NUM_THREADS 2
+#define BILLION 1000000000L
+
+void RowSwitch_pthread(int **A, int row);
+void *FindMax(void *thr_arg);
+void RowSwitch_sequential(int **A, int row);
+void PrintArray(int **A);
+
+struct thread_data {
+	int st;
+	int ed;
+  int column_num;
+};
+struct thread_data thread_data_array[NUM_THREADS];
+
+uint64_t time_findmaxp = 0;
+uint64_t time_findmaxs = 0;
+int maxrow= 0;
+
+pthread_t pthrd_id[NUM_THREADS]; 
+pthread_mutex_t mutexcomp;
+
+int **A;
 
 void main()
 {
+	int i, j;
   
+  A = (int **) malloc (N * sizeof(int *));
+  for ( i = 0; i < N; i++ )
+    A[i] = (int *) malloc (N * sizeof(int));
+    
+  srand(3);
+  
+  for ( i = 0; i < N; i++ )
+  	for ( j = 0; j < N; j++ )
+  		A[i][j] = rand()%100 - 50;
+  
+  //PrintArray(A);
+  
+  for (i = 0; i < N-1; i++ )
+    RowSwitch_pthread( A, i );
+  
+  //PrintArray(A);
+  
+  printf("Time elapsed to find all pivots in entire matrix    with pthread is %15llu ns\n", (long long unsigned)time_findmaxp);
+  
+  for ( i = 0; i < N; i++ )
+    for ( j = 0; j < N; j++ )
+      A[i][j] = rand()%100 - 50;
+
+  //PrintArray(A);
+  
+  for (i = 0; i < N-1; i++ )
+    RowSwitch_sequential( A, i );
+  
+  //PrintArray(A);
+  
+  printf("Time elapsed to find all pivots in entire matrix without pthread is %15llu ns\n", (long long unsigned)time_findmaxs);
+  
+  for ( i = 0; i < N; i++ )
+    free(A[i]);
+  
+  free(A);
+  
+  return;
+}
+
+void *FindMax(void *thr_arg) {
+    int i, j;
+    int max_row_num;
+    
+    struct thread_data *thr_data;
+    thr_data = (struct thread_data *) thr_arg;
+    
+    max_row_num = thr_data->st;
+
+    for (i = thr_data->st; i <= thr_data->ed; i++)
+      if ( abs(A[i][thr_data->column_num]) > abs(A[max_row_num][thr_data->column_num]) )
+        max_row_num = i;
+    
+    pthread_mutex_lock (&mutexcomp);
+    if ( abs(A[max_row_num][thr_data->column_num]) > abs(A[maxrow][thr_data->column_num]) )
+      maxrow = max_row_num;
+    pthread_mutex_unlock (&mutexcomp);
+    
+    pthread_exit(NULL);
+}
+
+void RowSwitch_pthread(int **A, int row) {
+  int i;
+  int thread_num = NUM_THREADS;
+  int max_row_num;
+  void *status;
+  
+  int temp;
+  
+  while ((N-row)/thread_num < 2)
+    thread_num = thread_num-1;
+  
+  pthread_attr_t attr; 
+
+  uint64_t diff;
+  struct timespec start, end;
+  
+  maxrow = row;
+  
+  pthread_mutex_init(&mutexcomp, NULL);
+  pthread_attr_init(&attr);
+  pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+  
+  clock_gettime(CLOCK_MONOTONIC, &start);	/* mark start time */
+  
+  for ( i = 0; i < thread_num; i++ ) {
+    thread_data_array[i].st = row + i * (N-row)/thread_num;
+    thread_data_array[i].ed = thread_data_array[i].st + (N-row)/thread_num -1;
+    if ( i >= thread_num - (N-row)%thread_num )
+      thread_data_array[i].ed = thread_data_array[i].ed + 1;
+      
+    thread_data_array[i].column_num = row;
+    
+    int err = pthread_create(&pthrd_id[i], &attr, FindMax, (void *) &thread_data_array[i]);
+    //printf("err is %d\n", err);
+  }
+  
+  pthread_attr_destroy(&attr);
+  
+  for(i = 0; i < thread_num; i++)
+  {
+    pthread_join(pthrd_id[i], &status);
+  }
+  
+  clock_gettime(CLOCK_MONOTONIC, &end);	/* mark the end time */
+  diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+  time_findmaxp = time_findmaxp + diff;
+  
+  pthread_mutex_destroy(&mutexcomp);
+  
+  if ( row != maxrow )
+    for ( i = 0; i < N; i++ ) {
+     	temp = A[row][i];
+      A[row][i] = A[maxrow][i];
+    	A[maxrow][i] = temp;
+    }
+  
+  return;
+}
+
+void RowSwitch_sequential(int **A, int row) {
+  int i = 0;
+  int max_row_num = row;
+  
+  int temp;
+
+  uint64_t diff;
+  struct timespec start, end;
+    
+  clock_gettime(CLOCK_MONOTONIC, &start);	/* mark start time */
+
+  for ( i = row; i < N; i++ )
+    if ( abs(A[i][row]) > abs(A[max_row_num][row]) )
+      max_row_num = i;
+
+  clock_gettime(CLOCK_MONOTONIC, &end);	/* mark the end time */
+  diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+  time_findmaxs = time_findmaxs + diff;
+
+  if (row != max_row_num )
+    for ( i = 0; i < N; i++ ) {
+      temp = A[row][i];
+      A[row][i] = A[max_row_num][i];
+      A[max_row_num][i] = temp;
+    }
+
+  return;
+}
+
+void PrintArray(int **A)
+{
+  int i,j;
+  
+  for ( i = 0; i < N; i++ ) {
+  	for ( j = 0; j < N; j++ )
+  		printf ("%3d  ", A[i][j]); 
+    printf("\n");
+  }
+  printf("\n");
+  
+  return;
 }
